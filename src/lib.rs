ec615aef352a374a81bd615c721fde3efd538af9
Cameron Brownsey <cameron@brownsey.com>
2025-09-17 18:25:25 +1200

    Added README.

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,4 @@
-//! Wrenlet is a set of rust bindings around the scripting language
-//! [Wren](https://wren.io), with the goal of providing safe and easy to use
-//! bindings.
-//!
-//! # Features
-//! `wrenlet` is currently a rather barebones library. As of this moment, it
-//! only supports Wren v0.4.0, and even then, it does not support all the
-//! features of the C implementation.
-//!
-//! Primarily, foreign methods and classes are not implemented, although this
-//! is a high priority. Additionally, `wrenlet` only supports passing or
-//! returning immutable or non-reference types.
-//!
-//! [Wren]: https://wren.io
+#![doc = include_str!("../README.md")]
 #![allow(dead_code)]
 
 pub mod error;
